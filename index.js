d0914e71452a00cb34b53d70b02779613936b334
jholt1 <jonathan.a.holt@gmail.com>
2016-10-31 01:37:46 +0000

    cms

--- index.js
+++ index.js
@@ -6,14 +6,4 @@ var treeForPublicHook = require('./lib/tree-for-public');
 module.exports = {
   name: 'ember-yaml-cms',
   treeForPublic: treeForPublicHook,
-
-  treeForVendor: function() {
-    var np = this.project.nodeModulesPath;
-    return np + '/ember-yaml-cms/vendor';
-  },
-
-  included: function(app) {
-    app.include('vendor/my-component.css');
-  }
-
 };
