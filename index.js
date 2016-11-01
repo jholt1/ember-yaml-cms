16f7ca60072f62f8dc8be10e257cdc2b1fb94a32
jholt1 <jonathan.a.holt@gmail.com>
2016-11-01 00:32:58 +0000

    working version

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 /* jshint node: true */
 'use strict';
 
+var glob = require('glob');
+var fs = require('fs');
+var yaml = require('yamljs');
+var writeFile = require('broccoli-file-creator');
+
 var treeForPublicHook = require('./lib/tree-for-public');
 
 module.exports = {
